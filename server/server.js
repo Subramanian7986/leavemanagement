81dcc0833b2fa175d3b626d7f216759719483f67
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-10-20 15:27:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://953622205045:Sri@22205045@mern.xa35g.mongodb.net/leave-management', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://953622205045:Sri@22205045@mern.xa35g.mongodb.net/leave-management?retryWrites=true&w=majority')
   .then(() => {
     console.log('Connected to MongoDB');
     insertAdminUser(); // Call the function to insert admin if not present
