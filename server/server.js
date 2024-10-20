54905780b3f477ef2bb14690293a403ac9a0fbe0
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-10-20 15:48:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect((process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     insertAdminUser(); // Call the function to insert admin if not present
