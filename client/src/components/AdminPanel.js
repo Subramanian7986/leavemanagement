86c8564f0f778f5683d263c22b4afcc34c557291
Subramanian V <146872098+Lovelysubramanian@users.noreply.github.com>
2024-10-22 22:32:50 +0530

    Update AdminPanel.js

--- client/src/components/AdminPanel.js
+++ client/src/components/AdminPanel.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 // Define the API base URL
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://leave-management-system-5wch.onrender.com' 
+    ? 'https://leavemanagement-qaub.onrender.com' 
     : 'http://localhost:10000'; 
 
 const AdminPanel = () => {
